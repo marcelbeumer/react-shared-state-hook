2cb6332d4d4a0acbb26ec2518852af33453f9ad8
Marcel Beumer <marcel@marcelbeumer.com>
2020-02-29 10:46:07 +0100

    Use babel and @testing-library/react

--- test/basic.test.tsx
+++ test/basic.test.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import test from 'tropic';
 import assert from 'assert';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { render } from '@testing-library/react';
 import { createSharedStateHook } from '..';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 const { deepStrictEqual: eq } = assert;
-const { mount } = Enzyme;
 
 const createBefore = (initialState: any) => {
   const [useSharedState, setSharedState] = createSharedStateHook(initialState);
@@ -31,8 +27,8 @@ const createBefore = (initialState: any) => {
 test('setSharedState updates local state ref', () => {
   const initalState = { a: 1, b: 2 };
   const { BasicComponent, setSharedState } = createBefore(initalState);
-  const wrapper = mount(<BasicComponent />);
-  eq(wrapper.find('.basic').text(), '1 + 2');
+  const { container } = render(<BasicComponent />);
+  eq(container.querySelector('.basic')?.innerHTML, '1 + 2');
   setSharedState({ a: 1, b: 3 });
-  eq(wrapper.find('.basic').text(), '1 + 3');
+  eq(container.querySelector('.basic')?.innerHTML, '1 + 3');
 });
--- test/setup.js
+++ test/setup.js
@@ -1,2 +1,5 @@
-require('ts-node/register');
 require('jsdom-global/register');
+require('@babel/register')({
+  presets: [['@babel/preset-env'], '@babel/preset-typescript', '@babel/preset-react'],
+  extensions: ['.js', '.jsx', '.ts', '.tsx', 'tsx']
+});
